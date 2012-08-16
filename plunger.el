5bc8cfe2fbd21e9f84ee0ac48b7e7f0cc80d49bc
Jonas Bernoulli <jonas@bernoul.li>
2012-08-16 12:20:26 +0200

    bump to 0.2.1

--- plunger.el
+++ plunger.el
@@ -4,7 +4,7 @@
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Created: 20120112
-;; Version: 0.2.0
+;; Version: 0.2.1
 ;; Status: beta
 ;; Homepage: http://github.com/tarsius/plunger
 ;; Keywords: compile, convenience, lisp
